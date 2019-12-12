ff0c2f12e2074538eea3a4789e2d177890283d63
Marcos Henrique Curtale Serafim <Marcos.Henrique@schumann.com.br>
2019-12-12 13:27:30 -0300

    ground changes

--- index.js
+++ index.js
@@ -83,9 +83,9 @@ function main() {
     }
 
     function dumpObject(obj, lines = [], isLast = true, prefix = '') {
-        const localPrefix = isLast ? 'â””â”€' : 'â”œâ”€';
+        const localPrefix = isLast ? '└─' : '├─';
         lines.push(`${prefix}${prefix ? localPrefix : ''}${obj.name || '*no-name*'} [${obj.type}]`);
-        const newPrefix = prefix + (isLast ? '  ' : 'â”‚ ');
+        const newPrefix = prefix + (isLast ? '  ' : '│ ');
         const lastNdx = obj.children.length - 1;
         obj.children.forEach((child, ndx) => {
             const isLast = ndx === lastNdx;
